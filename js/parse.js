c8950e32a77ebbeae9271ed1c8626da2df093acd
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-12-22 06:25:50 +0300

    Parse function parameters

--- js/parse.js
+++ js/parse.js
@@ -26,8 +26,6 @@ var parseC = function() {
             for(var type in tokenTypes) {
                 var match = tokenTypes[type].exec(s);
 
-                console.log(type + " " + match);
-
                 if(match) {
                     if(type !== 'WS') {
                         tokens.push({
@@ -43,6 +41,7 @@ var parseC = function() {
                 }
             }
 
+            // TODO: error messages
         } while(s.length !== 0 && foundToken);
 
         return tokens;
@@ -50,11 +49,100 @@ var parseC = function() {
 
     return function(s) {
         var parser = {
-            s: s,
+            tokens: tokenizeC(s),
             i: 0,
 
+            error: "",
+
             FUNCTION: function() {
-                return tokenizeC(s);
+                // TODO: error messages
+
+                var iOld = this.i;
+
+                if(!this.JUST_TOKEN('ID')) {
+                    this.i = iOld;
+
+                    return null;
+                }
+
+                if(!this.JUST_TOKEN('ID')) {
+                    this.i = iOld;
+
+                    return null;
+                }
+
+                if(!this.JUST_TOKEN('OPEN_PAREN')) {
+                    this.i = iOld;
+
+                    return null;
+                }
+
+                var params = this.PARAMS();
+
+                if(!this.JUST_TOKEN('CLOSE_PAREN')) {
+                    this.i = iOld;
+
+                    return null;
+                }
+
+                return [params, this.tokens.splice(this.i)];
+            },
+
+            JUST_TOKEN: function(type) {
+                return this.tokens[this.i++].type === type;
+            },
+
+            PARAMS: function() {
+                var iOld = this.i;
+
+                var firstParam = this.PARAM();
+
+                if(firstParam === null) {
+                    return [];
+                }
+
+                var params = [firstParam];
+
+                while(true) {
+                    if(!this.JUST_TOKEN('COMMA')) {
+                        this.i -= 1;
+                        break;
+                    }
+
+                    var nextParam = this.PARAM();
+
+                    if(nextParam === null) {
+                        this.i = iOld;
+                        return null;
+                    }
+
+                    params.push(nextParam);
+                }
+
+                return params;
+            },
+
+            PARAM: function() {
+                var iOld = this.i;
+
+                var varType = this.tokens[this.i++];
+
+                if(varType.type !== 'ID') {
+                    this.i = iOld;
+                    return null;
+                }
+
+                var varId = this.tokens[this.i++];
+
+                if(varId.type !== 'ID') {
+                    this.i = iOld;
+                    return null;
+                }
+
+                return {
+                    type: varType.s,
+                    id: varId.s
+                }
             }
         };
 
