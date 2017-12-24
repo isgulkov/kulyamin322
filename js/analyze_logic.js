7a35f829c07360c82f184b12caf743eba2f23dc9
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-12-24 17:12:57 +0300

    Display all combinations in the appropriate cells

--- js/analyze_logic.js
+++ js/analyze_logic.js
@@ -227,3 +227,7 @@ var findCombination = function(simpleExpressions, values) {
         return combination;
     }
 };
+
+var getCoveringSetMCDC = function(combinations, values) {
+    return combinations.filter(function(p1, p2, p3) { return p1 !== null; });
+};
